79ad7e5b7ae56407eff972b7705ec12f01446ae3
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-13 20:06:14 +0800

    add comments

--- generate_exp_variate.m
+++ generate_exp_variate.m
@@ -1,5 +1,11 @@
 function output = generate_exp_variate(row_num)
+    % After running some simulations, this was determined to be the
+    % best value for lambda that would yield values between [1,100]    
     lambda = 0.0482;
+    
+    % Exp. variate generator outputs floats but the range should only 
+    % support integers so we take the ceiling of the value.
+    % Then, we ensure the value is within [1,100] by clamping it with min,max
     R = rand(row_num, 1);
     X = -(log(1 - R)) / lambda;
     X = ceil(X);
--- lcg.m
+++ lcg.m
@@ -1,2 +1,4 @@
 function output = lcg(x, c, m, a)
+    % Mod 100 so the values don't go above 100
+    % This inherently makes LCG much less random no matter what values are used
     output = mod(mod(c * x + a, m), 100) + 1;
