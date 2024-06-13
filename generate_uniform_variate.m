718d896237e0701039b91b22bdbfbe599ad1bc32
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-13 17:10:33 +0800

    remove useless clamping in generate_uniform_variate

--- generate_uniform_variate.m
+++ generate_uniform_variate.m
@@ -2,8 +2,8 @@ function output = generate_uniform_variate(row_num)
     a = 1;
     b = 100;
    
+    % Take the ceiling to ensure integers
     R = rand(row_num, 1);
     X = a + R * (b - a);
     X = ceil(X);
-    X = min(max(X, a), b);
     output = X;
