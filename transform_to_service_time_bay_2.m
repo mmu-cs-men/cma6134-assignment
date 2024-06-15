ed1a4593ffafa673d4c7bb497f1a5b92339fb7c5
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-15 14:15:06 +0800

    remove extra useless service times

--- transform_to_service_time_bay_2.m
+++ transform_to_service_time_bay_2.m
@@ -1,6 +1,6 @@
 function output = transform_to_service_time_bay_2(col)
     cdf = [0.17 0.32 0.45 0.58 0.69 0.78 0.86 0.93 0.98 1];
-    service_times = [12 13 14 15 16 17 18 19 20 21 22];
+    service_times = [12 13 14 15 16 17 18 19 20 21];
     
     output = zeros(size(col));
     
--- transform_to_service_time_bay_3.m
+++ transform_to_service_time_bay_3.m
@@ -1,6 +1,6 @@
 function output = transform_to_service_time_bay_2(col)
     cdf = [0.02 0.07 0.14 0.22 0.31 0.42 0.55 0.7 0.87 1];
-    service_times = [12 13 14 15 16 17 18 19 20 21 22];
+    service_times = [12 13 14 15 16 17 18 19 20 21];
     
     output = zeros(size(col));
     
