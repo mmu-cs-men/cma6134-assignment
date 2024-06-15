6e4c57c7dff0deee63fb74662cb19016972d4ea6
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-15 14:16:30 +0800

    update service time printing times

--- print_service_time_table.m
+++ print_service_time_table.m
@@ -4,16 +4,16 @@ function print_service_time_table()
     printf('Wash Bay 1:\n');
     printf('Service Time     Probability     CDF      Range\n');
     printf('----------------------------------------------------------------\n');
-    printf('6 mins           0.05            0.05     1-5\n');
-    printf('7 mins           0.05            0.1      6-10\n');
-    printf('8 mins           0.1             0.2      11-20\n');
-    printf('9 mins           0.1             0.3      21-30\n');
-    printf('10 mins          0.2             0.5      31-50\n');
-    printf('11 mins          0.15            0.65     51-65\n');
-    printf('12 mins          0.15            0.8      66-80\n');
-    printf('13 mins          0.1             0.9      81-90\n');
-    printf('14 mins          0.05            0.95     91-95\n');
-    printf('15 mins          0.05            1        96-100\n');
+    printf('12 mins          0.05            0.05     1-5\n');
+    printf('13 mins          0.05            0.1      6-10\n');
+    printf('14 mins          0.1             0.2      11-20\n');
+    printf('15 mins          0.1             0.3      21-30\n');
+    printf('16 mins          0.2             0.5      31-50\n');
+    printf('17 mins          0.15            0.65     51-65\n');
+    printf('18 mins          0.15            0.8      66-80\n');
+    printf('19 mins          0.1             0.9      81-90\n');
+    printf('20 mins          0.05            0.95     91-95\n');
+    printf('21 mins          0.05            1        96-100\n');
     
     printf('\n\n');
     
@@ -21,16 +21,16 @@ function print_service_time_table()
     printf('Wash Bay 2:\n');
     printf('Service Time     Probability     CDF      Range\n');
     printf('----------------------------------------------------------------\n');
-    printf('6 mins           0.17            0.17     1-17\n');
-    printf('7 mins           0.15            0.32     18-32\n');
-    printf('8 mins           0.13            0.45     33-45\n');
-    printf('9 mins           0.13            0.58     46-58\n');
-    printf('10 mins          0.11            0.69     59-69\n');
-    printf('11 mins          0.09            0.78     70-78\n');
-    printf('12 mins          0.08            0.86     79-86\n');
-    printf('13 mins          0.07            0.93     87-93\n');
-    printf('14 mins          0.05            0.98     94-98\n');
-    printf('15 mins          0.02            1        99-100\n');
+    printf('12 mins          0.17            0.17     1-17\n');
+    printf('13 mins          0.15            0.32     18-32\n');
+    printf('14 mins          0.13            0.45     33-45\n');
+    printf('15 mins          0.13            0.58     46-58\n');
+    printf('16 mins          0.11            0.69     59-69\n');
+    printf('17 mins          0.09            0.78     70-78\n');
+    printf('18 mins          0.08            0.86     79-86\n');
+    printf('19 mins          0.07            0.93     87-93\n');
+    printf('20 mins          0.05            0.98     94-98\n');
+    printf('21 mins          0.02            1        99-100\n');
     
     printf('\n\n');
     
@@ -38,15 +38,15 @@ function print_service_time_table()
     printf('Wash Bay 3:\n');
     printf('Service Time     Probability     CDF      Range\n');
     printf('----------------------------------------------------------------\n');
-    printf('6 mins           0.02            0.02     1-2\n');
-    printf('7 mins           0.05            0.07     3-7\n');
-    printf('8 mins           0.07            0.14     8-14\n');
-    printf('9 mins           0.08            0.22     15-22\n');
-    printf('10 mins          0.09            0.31     23-31\n');
-    printf('11 mins          0.11            0.42     32-42\n');
-    printf('12 mins          0.13            0.55     43-55\n');
-    printf('13 mins          0.15            0.7      56-70\n');
-    printf('14 mins          0.17            0.87     71-87\n');
-    printf('15 mins          0.13            1        88-100\n');
+    printf('12 mins          0.02            0.02     1-2\n');
+    printf('13 mins          0.05            0.07     3-7\n');
+    printf('14 mins          0.07            0.14     8-14\n');
+    printf('15 mins          0.08            0.22     15-22\n');
+    printf('16 mins          0.09            0.31     23-31\n');
+    printf('17 mins          0.11            0.42     32-42\n');
+    printf('18 mins          0.13            0.55     43-55\n');
+    printf('19 mins          0.15            0.7      56-70\n');
+    printf('20 mins          0.17            0.87     71-87\n');
+    printf('21 mins          0.13            1        88-100\n');
     printf('----------------------------------------------------------------\n');
 end
